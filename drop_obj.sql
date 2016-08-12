61fb09ac8aecac6fe9533cf45842f844d0f00276
Andres Hernandez <andresrhernandezm@gmail.com>
2016-08-11 21:22:37 -0400

    deporte creado sin problema

--- drop_obj.sql
+++ drop_obj.sql
@@ -1,16 +1,16 @@
-DROP TYPE distancia_table
+DROP TYPE distancia_table FORCE
 /
-DROP TYPE deporte_t
+DROP TYPE deporte_t FORCE
 /
-DROP TYPE medalla_t
+DROP TYPE medalla_t FORCE
 /
-DROP TYPE competidor_t
+DROP TYPE competidor_t FORCE
 /
-DROP TYPE entrenador_t
+DROP TYPE entrenador_t FORCE
 /
-DROP TYPE deportista_t
+DROP TYPE deportista_t FORCE
 /
-DROP TYPE deportistas_t
+DROP TYPE deportistas_t FORCE
 /
-DROP TYPE distancia_t
+DROP TYPE distancia_t FORCE
 /
