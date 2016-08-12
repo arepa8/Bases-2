4b316474da8df95539b634ccc856e3713a41461b
Andres <ahernandez@jjbcompu-innovation.com>
2016-08-11 20:55:14 -0430

    drop file

---
+++ drop_obj.sql
@@ -0,0 +1,16 @@
+DROP TYPE deporte_t
+/
+DROP TYPE medalla_t
+/
+DROP TYPE competidor_t
+/
+DROP TYPE entrenador_t
+/
+DROP TYPE distancia_t
+/
+DROP TYPE distancia_table
+/
+DROP TYPE deportista_t
+/
+DROP TYPE deportistas_t
+/
