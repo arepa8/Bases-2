0829d1cb08f3790d4af7eda3ef6b1c49aae3ced3
Andres <ahernandez@jjbcompu-innovation.com>
2016-08-15 18:54:17 -0430

    seguimos probando

--- obj_rel.sql
+++ obj_rel.sql
@@ -1,36 +1,7 @@
-CREATE TYPE distancia_t 
+CREATE TYPE distancia AS VARRAY(50) OF INTEGER
 /
-CREATE TYPE distancia_table
-/
-CREATE TYPE deporte_t 
-/
-CREATE TYPE disciplina
-/
-CREATE TYPE medalla_t 
-/
-CREATE TYPE competidor_t
-/
-CREATE TYPE entrenador_t
-/
-CREATE TYPE deportista_t
-/
-CREATE TYPE deportista_table
-/
-
-CREATE TYPE distancia_t AS OBJECT
-           (course_no  NUMBER,
-            title      VARCHAR2(64),
-            credits    NUMBER);
-/
-CREATE TYPE distancia_table AS TABLE OF distancia_t;
-/
-
--- create deporte table
 CREATE TABLE deporte (
 	nombre		VARCHAR(40),
 	record		CHAR(8),
-	distancia	distancia_table,
-	MEMBER FUNCTION get_medalla RETURN INT,
-	MEMBER FUNCTION get_competidor RETURN VARCHAR
-	);
-/
+	distancia	distancia_table
+);
