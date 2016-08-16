43a6b78a2a88f30e57b4d2c2a1515db8a629e2af
Andres <ahernandez@jjbcompu-innovation.com>
2016-08-15 21:03:52 -0430

    mensajes 2

--- obj_rel.sql
+++ obj_rel.sql
@@ -15,43 +15,43 @@ CREATE OR REPLACE TYPE deportista_t
 CREATE OR REPLACE TYPE deportista_table
 /
 
-DBMS_OUTPUT.PUT_LINE("Distancia")
+DBMS_OUTPUT.PUT_LINE('Distancia')
 CREATE OR REPLACE TYPE distancia AS VARRAY(5) OF INTEGER
 /
-DBMS_OUTPUT.PUT_LINE("Deporte")
+DBMS_OUTPUT.PUT_LINE('Deporte')
 CREATE TABLE deporte (
 	nombre		VARCHAR(40),
 	record		CHAR(8),
 	distancia	distancia
 );
-DBMS_OUTPUT.PUT_LINE("medalla_t")
+DBMS_OUTPUT.PUT_LINE('medalla_t')
 CREATE OR REPLACE TYPE medalla_t AS OBJECT (
 	id			INT,
 	olimpiada	VARCHAR(20),
 	otorga REF 	deporte_t);
 /
-DBMS_OUTPUT.PUT_LINE("medalla")
+DBMS_OUTPUT.PUT_LINE('medalla')
 CREATE TABLE medalla OF medalla_t;
 
-DBMS_OUTPUT.PUT_LINE("Deporte_t")
+DBMS_OUTPUT.PUT_LINE('Deporte_t')
 CREATE OR REPLACE TYPE deporte_t AS OBJECT (
 	MEMBER FUNCTION get_medalla RETURN medalla_t
 	MEMBER FUNCTION get_competidor RETURN competidor_t
 );
 /
 
-DBMS_OUTPUT.PUT_LINE("Disciplina")
+DBMS_OUTPUT.PUT_LINE('Disciplina')
 CREATE OR REPLACE TYPE disciplina AS TABLE OF REF deporte_t
 /
 
-DBMS_OUTPUT.PUT_LINE("competidor_t")
+DBMS_OUTPUT.PUT_LINE('competidor_t')
 CREATE OR REPLACE TYPE competidor_t AS OBJECT (
 	carnet	INT,
 	nombre	VARCHAR(40),
 	compite	disciplina) NOT FINAL;
 /
 
-DBMS_OUTPUT.PUT_LINE("entrenador_t")
+DBMS_OUTPUT.PUT_LINE('entrenador_t')
 CREATE OR REPLACE TYPE entrenador_t UNDER competidor_t (
 	edad				INT,
 	tiempo_experiencia	INT,
@@ -59,7 +59,7 @@ CREATE OR REPLACE TYPE entrenador_t UNDER competidor_t (
 	MEMBER FUNCTION get_deportistas RETURN deportista_table);
 /
 
-DBMS_OUTPUT.PUT_LINE("deportista_t")
+DBMS_OUTPUT.PUT_LINE('deportista_t')
 CREATE OR REPLACE TYPE deportista_t UNDER competidor_t (
 	pais 		VARCHAR(20),
 	edad 		INT,
@@ -67,11 +67,11 @@ CREATE OR REPLACE TYPE deportista_t UNDER competidor_t (
 	entrena REF entrenador_t);
 /
 
-DBMS_OUTPUT.PUT_LINE("deportista_table")
+DBMS_OUTPUT.PUT_LINE('deportista_table')
 CREATE OR REPLACE TYPE deportista_table AS TABLE OF REF deportista_t
 /
 
-DBMS_OUTPUT.PUT_LINE("competidor")
+DBMS_OUTPUT.PUT_LINE('competidor')
 CREATE TABLE competidor OF competidor_t
 	NESTED TABLE compite STORE AS compite_nt;
 
