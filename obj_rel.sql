b98c192815ccdd03a792e5307627b437696b397a
Andres <ahernandez@jjbcompu-innovation.com>
2016-08-15 22:50:00 -0430

    arreglando

--- obj_rel.sql
+++ obj_rel.sql
@@ -19,9 +19,7 @@ CREATE TABLE deporte (
 	distancia 	distancias
 );
 
-INSERT INTO deporte (nombre, record, distancia) VALUES ('Natacion','10.08s',distancias(100,200,400,800))
-INSERT INTO deporte (nombre, record, distancia) VALUES ('Atletismo','9.54s',distancias(100,200,400,800,1500))
-INSERT INTO deporte (nombre, record, distancia) VALUES ('Ciclismo','10.08s',distancias(10000,20000,40000,80000))
+
 
 CREATE OR REPLACE TYPE medalla_t AS OBJECT (
 	id			INT,
@@ -30,9 +28,6 @@ CREATE OR REPLACE TYPE medalla_t AS OBJECT (
 /
 CREATE TABLE medalla OF medalla_t;
 
-INSERT INTO medalla (id,olimpiada,otorga) VALUES (1,'Beijing',SELECT REF(d) FROM deporte d WHERE d.nombre='Natacion')
-INSERT INTO medalla (id,olimpiada,otorga) VALUES (1,'Londres',SELECT REF(d) FROM deporte d WHERE d.nombre='Atletismo')
-INSERT INTO medalla (id,olimpiada,otorga) VALUES (1,'Rio',SELECT REF(d) FROM deporte d WHERE d.nombre='Ciclismo')
 
 CREATE OR REPLACE TYPE deporte_t AS OBJECT (
 	nombre		VARCHAR(40),
@@ -76,4 +71,14 @@ CREATE TYPE deportista_table AS TABLE OF deportista_t
 CREATE TABLE competidor OF competidor_t
 	NESTED TABLE compite STORE AS compite_nt;
 
+BEGIN
+	INSERT INTO deporte (nombre, record, distancia) VALUES ('Natacion','10.08s',distancias(100,200,400,800))
+	INSERT INTO deporte (nombre, record, distancia) VALUES ('Atletismo','9.54s',distancias(100,200,400,800,1500))
+	INSERT INTO deporte (nombre, record, distancia) VALUES ('Ciclismo','10.08s',distancias(10000,20000,40000,80000))
+
+	INSERT INTO medalla (id,olimpiada,otorga) VALUES (1,'Beijing',SELECT REF(d) FROM deporte d WHERE d.nombre='Natacion')
+	INSERT INTO medalla (id,olimpiada,otorga) VALUES (1,'Londres',SELECT REF(d) FROM deporte d WHERE d.nombre='Atletismo')
+	INSERT INTO medalla (id,olimpiada,otorga) VALUES (1,'Rio',SELECT REF(d) FROM deporte d WHERE d.nombre='Ciclismo')
+END;
+/
 
